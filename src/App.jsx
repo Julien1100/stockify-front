78472e67ca7412e6a586efc2247e99c904736e79
J8T <jul.milcent@gmail.com>
2024-03-05 13:47:11 +0100

    🔄 Chore: Update package dependencies
    📦 This commit updates package dependencies in `package.json` and `package-lock.json`.
    
    🔧 Changes:
    - Added `"axios": "^1.6.7"` to dependencies.
    - Added `"react-router-dom": "^6.22.0"` to dependencies.
    
    🗑️ Remove unused files
    
    🧹 This commit removes unused files from the project.
    
    🔧 Changes:
    - Removed `App.css` as it is no longer needed.
    - Updated `App.jsx` to integrate `react-router-dom` for routing.
    - Added `HomePage.jsx` to the `src/pages/` folder.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
-import "./App.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage />,
+  },
+]);
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  );
 }
 
 export default App;
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function HomePage() {
+  return (
+    <div>
+      <p>Hello</p>
+    </div>
+  );
+}
+
+export default HomePage;
