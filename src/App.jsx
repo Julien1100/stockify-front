76419b88f185e9af9b43b7ca339b55f3e0b9d7d6
J8T <jul.milcent@gmail.com>
2024-03-06 17:09:25 +0100

    Add profile and create page

--- src/App.jsx
+++ src/App.jsx
@@ -7,12 +7,16 @@ import {
 
 // layouts and pages
 import Dashboard from "./pages/Dashboard";
+import Create from "./pages/Create";
+import Profile from "./pages/Profile";
 import RootLayout from "./layouts/RootLayout";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<RootLayout />}>
       <Route index element={<Dashboard />} />
+      <Route path="create" element={<Create />} />
+      <Route path="profile" element={<Profile />} />
     </Route>
   )
 );
