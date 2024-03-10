a10d436ddbf4e42e2a692596f6a30d896d7fc495
J8T <jul.milcent@gmail.com>
2024-03-10 17:00:30 +0100

    :building_construction: Add ProductDetails page
    Added the ProductDetails page component and integrated it into the routing configuration. Now users can navigate to a specific product details page by clicking on a product in the Dashboard.
    
    Changes:
    - Added `ProductDetails.jsx` in the `src/pages/` directory.
    - Updated `App.jsx` to include routing for the ProductDetails page.
    - Imported `useNavigate` from `react-router-dom` in `Dashboard.jsx` to enable navigation to the ProductDetails page.
    - Implemented clickable rows in the Dashboard table to navigate to the details page when a product is clicked.

---
+++ src/pages/ProductDetails.jsx
@@ -0,0 +1,116 @@
+import {
+  Box,
+  Card,
+  CardBody,
+  CardHeader,
+  Container,
+  HStack,
+  Heading,
+  Stack,
+  StackDivider,
+  Text,
+} from "@chakra-ui/react";
+
+import { useLoaderData, useParams } from "react-router-dom";
+
+export default function ProductDetails() {
+  // const { id } = useParams();
+  const product = useLoaderData();
+
+  return (
+    <Container>
+      <Card my={10}>
+        <CardHeader>
+          <Heading size={"lg"} textTransform={"capitalize"}>
+            {product.name}
+          </Heading>
+        </CardHeader>
+
+        <CardBody>
+          <Stack divider={<StackDivider />} spacing={4}>
+            <Box>
+              <Heading size={"xs"} textTransform={"uppercase"}>
+                Description
+              </Heading>
+              <Text pt={2} fontSize={"sm"}>
+                {product.description}
+              </Text>
+            </Box>
+
+            <HStack justifyContent={"space-between"}>
+              <Box>
+                <Heading size={"xs"} textTransform={"uppercase"}>
+                  Quantité en stock
+                </Heading>
+                <Text pt={2} fontSize={"sm"}>
+                  {product.quantityInStock}
+                </Text>
+              </Box>
+
+              <Box>
+                <Heading size={"xs"} textTransform={"uppercase"}>
+                  Quantité totale
+                </Heading>
+                <Text pt={2} fontSize={"sm"}>
+                  {product.quantityTotal}
+                </Text>
+              </Box>
+            </HStack>
+
+            <HStack justifyContent={"space-between"}>
+              <Box>
+                <Heading size={"xs"} textTransform={"uppercase"}>
+                  Batteries ?
+                </Heading>
+                <Text pt={2} fontSize={"sm"}>
+                  {product.needBattery ? "Oui" : "Non"}
+                </Text>
+              </Box>
+
+              <Box>
+                <Heading size={"xs"} textTransform={"uppercase"}>
+                  Congelé ?
+                </Heading>
+                <Text pt={2} fontSize={"sm"}>
+                  {product.frozen ? "Oui" : "Non"}
+                </Text>
+              </Box>
+
+              <Box>
+                <Heading size={"xs"} textTransform={"uppercase"}>
+                  Date d'expiration ?
+                </Heading>
+                <Text pt={2} fontSize={"sm"}>
+                  {product.hasExpirationDate ? "Oui" : "Non"}
+                </Text>
+              </Box>
+            </HStack>
+
+            <Box>
+              <Heading size={"xs"} textTransform={"uppercase"}>
+                Stockage
+              </Heading>
+              <Text pt={2} fontSize={"sm"}>
+                {product.location ? mapLocation(product.location) : "N/A"}
+              </Text>
+            </Box>
+          </Stack>
+        </CardBody>
+      </Card>
+    </Container>
+  );
+}
+
+// Map pour remplacer les valeurs de location
+const mapLocation = (location) => {
+  switch (location) {
+    case "":
+      return "N/A";
+    case "shelf":
+      return "Étagère";
+    case "freezer":
+      return "Congélateur";
+    default:
+      return location;
+  }
+};
