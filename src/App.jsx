0e13fcea77c2628de81fd126eb0ea80673dfc2c0
J8T <jul.milcent@gmail.com>
2024-03-21 12:53:06 +0100

    test vercel

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
-import {
-  Route,
-  RouterProvider,
-  createBrowserRouter,
-  createRoutesFromElements,
-} from "react-router-dom";
+// import {
+//   Route,
+//   RouterProvider,
+//   createBrowserRouter,
+//   createRoutesFromElements,
+// } from "react-router-dom";
+
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 // layouts
 import RootLayout from "./layouts/RootLayout";
@@ -22,42 +24,48 @@ import { productsLoader } from "./services/productsLoader";
 import { productDetailsLoader } from "./services/productLoader";
 import { userLoader } from "./services/userLoader";
 import { registerAction } from "./services/registerAction";
+import Login from "./components/Login";
 
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path="/">
-      <Route path="login" element={<LoginLayout />} />
-      <Route
-        path="register"
-        element={<RegisterLayout />}
-        action={registerAction}
-      />
-      <Route element={<RootLayout />} loader={userLoader}>
-        <Route index element={<Dashboard />} loader={productsLoader} />
-        <Route path="products">
-          <Route
-            path=":id"
-            element={<ProductDetails />}
-            loader={productDetailsLoader}
-          />
-          <Route path="new" element={<NewProduct />} />
-          <Route
-            path=":id/edit"
-            element={<UpdateProduct />}
-            loader={productDetailsLoader}
-          />
-        </Route>
-        <Route path="profile" element={<Profile />} loader={userLoader} />
-      </Route>
-    </Route>
-  )
-);
+// const router = createBrowserRouter(
+//   createRoutesFromElements(
+//     <Route path="/">
+//       <Route path="login" element={<LoginLayout />} />
+//       <Route
+//         path="register"
+//         element={<RegisterLayout />}
+//         action={registerAction}
+//       />
+//       <Route element={<RootLayout />} loader={userLoader}>
+//         <Route index element={<Dashboard />} loader={productsLoader} />
+//         <Route path="products">
+//           <Route
+//             path=":id"
+//             element={<ProductDetails />}
+//             loader={productDetailsLoader}
+//           />
+//           <Route path="new" element={<NewProduct />} />
+//           <Route
+//             path=":id/edit"
+//             element={<UpdateProduct />}
+//             loader={productDetailsLoader}
+//           />
+//         </Route>
+//         <Route path="profile" element={<Profile />} loader={userLoader} />
+//       </Route>
+//     </Route>
+//   )
+// );
 
 function App() {
   return (
-    <>
-      <RouterProvider router={router} />
-    </>
+    // <>
+    //   <RouterProvider router={router} />
+    // </>
+    <Router>
+      <Routes>
+        <Route path="/" exact element={<LoginLayout />} />
+      </Routes>
+    </Router>
   );
 }
 
