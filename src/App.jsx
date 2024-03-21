49a582c99cad62d2e06609a4ccb43ede63f5baad
J8T <jul.milcent@gmail.com>
2024-03-21 14:28:52 +0100

    test with vercel.json in public folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-// import {
-//   Route,
-//   RouterProvider,
-//   createBrowserRouter,
-//   createRoutesFromElements,
-// } from "react-router-dom";
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
 
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+// import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 // layouts
 import RootLayout from "./layouts/RootLayout";
@@ -24,48 +24,67 @@ import { productsLoader } from "./services/productsLoader";
 import { productDetailsLoader } from "./services/productLoader";
 import { userLoader } from "./services/userLoader";
 import { registerAction } from "./services/registerAction";
-import Login from "./components/Login";
 
-// const router = createBrowserRouter(
-//   createRoutesFromElements(
-//     <Route path="/">
-//       <Route path="login" element={<LoginLayout />} />
-//       <Route
-//         path="register"
-//         element={<RegisterLayout />}
-//         action={registerAction}
-//       />
-//       <Route element={<RootLayout />} loader={userLoader}>
-//         <Route index element={<Dashboard />} loader={productsLoader} />
-//         <Route path="products">
-//           <Route
-//             path=":id"
-//             element={<ProductDetails />}
-//             loader={productDetailsLoader}
-//           />
-//           <Route path="new" element={<NewProduct />} />
-//           <Route
-//             path=":id/edit"
-//             element={<UpdateProduct />}
-//             loader={productDetailsLoader}
-//           />
-//         </Route>
-//         <Route path="profile" element={<Profile />} loader={userLoader} />
-//       </Route>
-//     </Route>
-//   )
-// );
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/">
+      <Route path="login" element={<LoginLayout />} />
+      <Route
+        path="register"
+        element={<RegisterLayout />}
+        action={registerAction}
+      />
+      <Route element={<RootLayout />} loader={userLoader}>
+        <Route index element={<Dashboard />} loader={productsLoader} />
+        <Route path="products">
+          <Route
+            path=":id"
+            element={<ProductDetails />}
+            loader={productDetailsLoader}
+          />
+          <Route path="new" element={<NewProduct />} />
+          <Route
+            path=":id/edit"
+            element={<UpdateProduct />}
+            loader={productDetailsLoader}
+          />
+        </Route>
+        <Route path="profile" element={<Profile />} loader={userLoader} />
+      </Route>
+    </Route>
+  )
+);
 
 function App() {
   return (
-    // <>
-    //   <RouterProvider router={router} />
-    // </>
-    <Router>
-      <Routes>
-        <Route path="/" exact element={<LoginLayout />} />
-      </Routes>
-    </Router>
+    <>
+      <RouterProvider router={router} />
+    </>
+    //   <Router>
+    //     <Routes>
+    //       <Route path="/" element={<RootLayout />} loader={userLoader}>
+    //         <Route path="login" element={<LoginLayout />} />
+    //         <Route
+    //           path="register"
+    //           element={<RegisterLayout />}
+    //           action={registerAction}
+    //         />
+    //         <Route path="profile" element={<Profile />} />
+    //         <Route index element={<Dashboard />} loader={productsLoader} />
+    //         <Route path="products/new" element={<NewProduct />} />
+    //         <Route
+    //           path="products/:id"
+    //           element={<ProductDetails />}
+    //           loader={productDetailsLoader}
+    //         />
+    //         <Route
+    //           path="products/:id/edit"
+    //           element={<UpdateProduct />}
+    //           loader={productDetailsLoader}
+    //         />
+    //       </Route>
+    //     </Routes>
+    //   </Router>
   );
 }
 
