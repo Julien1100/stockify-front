4a7c787a88277b74cda76d9b49822009389c1843
J8T <jul.milcent@gmail.com>
2024-04-16 16:05:56 +0200

    ✨ Updated userLoader
    Added userUpdate function

--- src/services/userLoader.js
+++ src/services/userLoader.js
@@ -41,3 +41,39 @@ export const userLoader = async () => {
     throw error;
   }
 };
+
+export const userUpdate = async ({ user }) => {
+  try {
+    // Récupérer le token depuis le localStorage
+    const token = localStorage.getItem("token");
+
+    if (!token) {
+      throw new Error("Token not found");
+    }
+
+    // Configurer les headers avec le token d'authentification
+    const config = {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    };
+
+    // Effectuer la requête PATCH avec Axios pour mettre à jour le produit
+    const response = await axios.put(
+      `${import.meta.env.VITE_API_URL}/user/update/${user._id}`,
+      user,
+      config
+    );
+
+    // Vérifier la réponse et retourner les données mises à jour
+    if (response.data) {
+      return response.data;
+    } else {
+      throw new Error("Failed to update user");
+    }
+  } catch (error) {
+    // Gérer les erreurs
+    console.error("Error updating user:", error);
+    throw error;
+  }
+};
