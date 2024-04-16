31dd9938eafc2989f55fe2a62d1c993e35ae6599
J8T <jul.milcent@gmail.com>
2024-04-16 11:26:44 +0200

    🛠️ Add Stockify logo to the registration page

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -19,6 +19,7 @@ import {
   ButtonGroup,
   Box,
   useToast,
+  Image,
 } from "@chakra-ui/react";
 import { registerAction } from "../services/registerAction";
 
@@ -72,98 +73,103 @@ export default function Register() {
   };
 
   return (
-    <Center h={"100vh"}>
-      <Container>
-        <Card>
-          <CardHeader>
-            <Heading>Créer un compte</Heading>
-          </CardHeader>
-
-          <CardBody>
-            <form onSubmit={handleSubmit}>
-              <FormControl>
-                <Flex justifyContent={"space-between"}>
-                  <Box>
-                    <FormLabel>Prénom</FormLabel>
-                    <Input
-                      type="text"
-                      name="firstName"
-                      placeholder="Entrez votre prénom…"
-                      value={formData.firstName}
-                      onChange={handleChange}
-                      mb={4}
-                      isRequired
-                    />
-                  </Box>
-
-                  <Box>
-                    <FormLabel>Nom</FormLabel>
-                    <Input
-                      type="text"
-                      name="lastName"
-                      placeholder="Entrez votre nom…"
-                      value={formData.lastName}
-                      onChange={handleChange}
-                      mb={4}
-                      isRequired
-                    />
-                  </Box>
-                </Flex>
-
-                <FormLabel>Adresse mail</FormLabel>
-                <Input
-                  type="email"
-                  name="email"
-                  placeholder="Entrez votre adresse mail…"
-                  value={formData.email}
-                  onChange={handleChange}
-                  mb={error ? 0 : 4}
-                  isRequired
-                  isInvalid={error}
-                  errorBorderColor="red.400"
-                />
-                {error && (
-                  <FormHelperText mb={4} color={"red.400"}>
-                    {error}
-                  </FormHelperText>
-                )}
-
-                <FormLabel>Mot de passe</FormLabel>
-                <InputGroup mb={4}>
-                  <Input
-                    type={show ? "text" : "password"}
-                    name="password"
-                    placeholder="Entrez votre mot de passe…"
-                    value={formData.password}
-                    onChange={handleChange}
-                    pr={"4.5rem"}
-                    mb={4}
-                    isRequired
-                  />
-                  <InputRightElement w={"4.5rem"}>
-                    <Button
-                      colorScheme="blue"
-                      h={"1.75rem"}
-                      size="sm"
-                      onClick={handleClick}
-                    >
-                      {show ? "Hide" : "Show"}
-                    </Button>
-                  </InputRightElement>
-                </InputGroup>
-
-                <ButtonGroup gap={4} colorScheme="blue">
-                  <Button type="submit">Enregistrer</Button>
-
-                  <Button variant={"ghost"}>
-                    <Link to="/login">Se connecter</Link>
-                  </Button>
-                </ButtonGroup>
-              </FormControl>
-            </form>
-          </CardBody>
-        </Card>
+    <>
+      <Container mb={6}>
+        <Image src="../../public/logo.png" alt="Logo de Stockify" />
       </Container>
-    </Center>
+      <Center>
+        <Container>
+          <Card>
+            <CardHeader>
+              <Heading>Créer un compte</Heading>
+            </CardHeader>
+
+            <CardBody>
+              <form onSubmit={handleSubmit}>
+                <FormControl>
+                  <Flex justifyContent={"space-between"}>
+                    <Box>
+                      <FormLabel>Prénom</FormLabel>
+                      <Input
+                        type="text"
+                        name="firstName"
+                        placeholder="Entrez votre prénom…"
+                        value={formData.firstName}
+                        onChange={handleChange}
+                        mb={4}
+                        isRequired
+                      />
+                    </Box>
+
+                    <Box>
+                      <FormLabel>Nom</FormLabel>
+                      <Input
+                        type="text"
+                        name="lastName"
+                        placeholder="Entrez votre nom…"
+                        value={formData.lastName}
+                        onChange={handleChange}
+                        mb={4}
+                        isRequired
+                      />
+                    </Box>
+                  </Flex>
+
+                  <FormLabel>Adresse mail</FormLabel>
+                  <Input
+                    type="email"
+                    name="email"
+                    placeholder="Entrez votre adresse mail…"
+                    value={formData.email}
+                    onChange={handleChange}
+                    mb={error ? 0 : 4}
+                    isRequired
+                    isInvalid={error}
+                    errorBorderColor="red.400"
+                  />
+                  {error && (
+                    <FormHelperText mb={4} color={"red.400"}>
+                      {error}
+                    </FormHelperText>
+                  )}
+
+                  <FormLabel>Mot de passe</FormLabel>
+                  <InputGroup mb={4}>
+                    <Input
+                      type={show ? "text" : "password"}
+                      name="password"
+                      placeholder="Entrez votre mot de passe…"
+                      value={formData.password}
+                      onChange={handleChange}
+                      pr={"4.5rem"}
+                      mb={4}
+                      isRequired
+                    />
+                    <InputRightElement w={"4.5rem"}>
+                      <Button
+                        colorScheme="blue"
+                        h={"1.75rem"}
+                        size="sm"
+                        onClick={handleClick}
+                      >
+                        {show ? "Hide" : "Show"}
+                      </Button>
+                    </InputRightElement>
+                  </InputGroup>
+
+                  <ButtonGroup gap={4} colorScheme="blue">
+                    <Button type="submit">Enregistrer</Button>
+
+                    <Button variant={"ghost"}>
+                      <Link to="/login">Se connecter</Link>
+                    </Button>
+                  </ButtonGroup>
+                </FormControl>
+              </form>
+            </CardBody>
+          </Card>
+        </Container>
+      </Center>
+    </>
   );
 }
